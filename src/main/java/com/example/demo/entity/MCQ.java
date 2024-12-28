29b0facac2e638d04aa9ffe888e59583050c431a
tushar26g <tushar26g@gmail.com>
2024-12-28 14:16:20 +0530

    Modified MCQ and application.properties

--- src/main/java/com/example/demo/entity/MCQ.java
+++ src/main/java/com/example/demo/entity/MCQ.java
@@ -8,6 +8,62 @@ import java.util.List;
 @Data
 @Document(collection = "MCQ")
 public class MCQ {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Id
     private String id;
     private String key;
